f2bc1303526c76609a83f14713df5d72dda4bdb9
Marcelo Casamassa <marcelocasamassa@gmail.com>
2024-03-13 09:49:08 -0300

    Initial version

---
+++ sketch-fire-siren/sketch_sirenebombeiro.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  tone(3, 264);
+  delay(1000);
+  tone(3, 2112);
+  delay(1000);
+}
